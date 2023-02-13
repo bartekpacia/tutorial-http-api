18273fdb1be09e8b9b26d5a4262983b625c4bbbf
Bartek Pacia <barpac02@gmail.com>
2023-02-14 00:23:42 +0100

    update ktor plugin so it fixed Gradle deprecation warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,6 @@
-val ktorVersion: String by project
-val kotlinVersion: String by project
-val logbackVersion: String by project
-
 plugins {
     kotlin("jvm") version "1.8.0"
-    id("io.ktor.plugin") version "2.2.2"
+    id("io.ktor.plugin") version "2.2.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
 }
 
@@ -26,11 +22,11 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    implementation("io.ktor:ktor-server-core-jvm:2.2.3")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.2.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.2.3")
+    implementation("io.ktor:ktor-server-netty-jvm:2.2.3")
+    implementation("ch.qos.logback:logback-classic:1.4.4")
+    testImplementation("io.ktor:ktor-server-tests-jvm:2.2.3")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.8.0")
 }
